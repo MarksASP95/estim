8dff35bad149a121d72ce5aec9a5dfd2fb766410
Marco Suárez <marcosuarezp95@gmail.com>
2020-10-03 17:27:14 -0400

    Show projects in the front page

--- src/components/EstimLayout/EstimLayout.tsx
+++ src/components/EstimLayout/EstimLayout.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
-import { Layout, Menu } from 'antd';
+import React, { ReactNode, useState, useEffect } from 'react'
+import { Layout, Menu, Row, Col, Button } from 'antd';
 import {
     AppstoreOutlined,
     TeamOutlined,
     BarsOutlined,
+    PlusOutlined,
 } from '@ant-design/icons';
 
 import {
@@ -11,6 +12,8 @@ import {
     Route,
     Link
   } from 'react-router-dom';
+
+import axios from 'axios'
 
 import ProjectList from '../ProjectList/ProjectList';
 import Project from '../../models/Project';
@@ -21,55 +24,48 @@ import './EstimLayout.scss';
 const { Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
-const mockProjects: Project[] = [{
-    "name": "Secured bi-directional parallelism",
-    "active": false,
-    "about": "Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque.",
-    "dueDate": new Date(10/18/2020),
-    "image": "https://i.picsum.photos/id/409/400/250.jpg?hmac=EOCapY_Ek48Mech0Iw2b3lOWCtI-UhJtMKmpywPfnYs"
-  }, {
-    "name": "Business-focused systematic structure",
-    "active": true,
-    "about": "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
-    "dueDate": new Date(10/19/2019),
-    "image": "https://i.picsum.photos/id/404/400/250.jpg?hmac=xJTG8LZN89EueWcRxvM9TG2ES574uE5O4dNjXUliOTE"
-  }, {
-    "name": "Upgradable object-oriented forecast",
-    "active": false,
-    "about": "Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue.",
-    "dueDate": new Date(9/28/2020),
-    "image": "https://i.picsum.photos/id/416/400/250.jpg?hmac=Fc0RSOWfGtw66wVXalAAPZNymkJr-z3l2IKzjwgrcLM"
-  }, {
-    "name": "Decentralized methodical moderator",
-    "active": true,
-    "about": "Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero.",
-    "dueDate": new Date(10/25/2020),
-    "image": "https://i.picsum.photos/id/615/400/250.jpg?hmac=B5_u6j9JLqoUF1qQA5BaxOkCyLsYxFg_kZXOBd5PZRw"
-  }, {
-    "name": "Switchable intermediate installation",
-    "active": false,
-    "about": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend.",
-    "dueDate": new Date(5/22/2020),
-    "image": "https://i.picsum.photos/id/866/400/250.jpg?hmac=9HDAr7aHm2NgjIg2mYqO4PAn6ZmgU1_nCxAzjmYZUK8"
-  }, {
-    "name": "Multi-channelled static paradigm",
-    "active": false,
-    "about": "Proin at turpis a pede posuere nonummy.",
-    "dueDate": new Date(5/15/2020),
-    "image": "https://i.picsum.photos/id/960/400/250.jpg?hmac=OQJ9PecVtGEbWRLdfAq7fJt5ODEHJpJOhdpnLtd6oT8"
-  }, {
-    "name": "Re-contextualized heuristic approach",
-    "active": false,
-    "about": "Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat.",
-    "dueDate": new Date(5/7/2020),
-    "image": "https://i.picsum.photos/id/867/400/250.jpg?hmac=Epi1Qtm_jrqx4klqVzXW5nZKHzGlK5DbPikBjxhCNfA",
-    "group": {
-        name: "Odalys",
-        image: "http://marco-dev.surge.sh/static/media/odalys.a2560ece.jpg"
-    }
-  }]
-
 const EstimLayout = () => {
+
+    const initialProjectsState = {
+        projects: [],
+        projectsError: false,
+        loadingProjects: true,
+    }
+    const [projectsState, setProjectsState] = useState(initialProjectsState)
+
+    const getProjects = async () => {
+
+        axios.get('http://localhost:5000/projects')
+        .then(res => {
+            console.log(res.data)
+            setProjectsState({
+                ...projectsState,
+                projects: res.data,
+                projectsError: false,
+                loadingProjects: false,
+            });
+        })
+        .catch(err => {
+            console.log('Error getting projects', err);
+            setProjectsState({
+                ...projectsState,
+                projectsError: true,
+                loadingProjects: false,
+            });
+        })
+    }
+
+    useEffect(() => {
+        getProjects()
+    }, []);
+
+    const getProjectsTemplateOutcome = (): ReactNode => {
+        if (projectsState.projectsError) return (<p>Something went wrong!</p>)
+
+        if (projectsState.loadingProjects) return (<p>Loading projects...</p>)
+
+        return <ProjectList projects={projectsState.projects} />
+    }
 
     const onCollapse = (collapsed: boolean) => {
         setLayoutState({
@@ -114,7 +110,7 @@ const EstimLayout = () => {
                                 <NewProject />
                             </Route>
                             <Route exact path="/">
-                                <ProjectList projects={mockProjects} />
+                                { getProjectsTemplateOutcome }
                             </Route>
 
                         </Switch>
